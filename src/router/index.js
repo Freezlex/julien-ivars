d76af8a117cd66eb24d3ff9f0b63bbdf4eb89d76
Ivars Julien <Sonyc098@gmail.com>
2021-03-31 12:20:08 +0200

    🖥️ | Fix router - 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home'
 import PageNotFound from "@/views/PageNotFound";
 
 Vue.use(VueRouter)
 
-  const routes = [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    { path: '/discord',
-      beforeEnter(){
-        window.location = "https://www.discord.gg/mFQMxqx";
-      }
-    },
-    { path: '/twitter',
-      beforeEnter(){
-        window.location = "https://twitter.com/freezlex";
-      }
-    },
-    { path: '/github',
-      beforeEnter(){
-        window.location = "https://github.com/freezlex";
-      }
-    },
-    { path: '/youtube',
-      beforeEnter(){
-        window.location = "https://www.youtube.com/channel/UCkExyZZOlvhZv_3h_HaPYYw";
-      }
-    },
-    { path: '/:pathMatch(.*)*', component: PageNotFound }
-]
-
 const router = new VueRouter({
   mode: 'history',
-  routes: routes
-})
+  base: __dirname
+  routes: [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/discord', beforeEnter(){ window.location = "https://www.discord.gg/mFQMxqx"; }},
+    { path: '/twitter', beforeEnter(){ window.location = "https://twitter.com/freezlex"; }},
+    { path: '/github', beforeEnter(){ window.location = "https://github.com/freezlex"; }},
+    { path: '/youtube', beforeEnter(){ window.location = "https://www.youtube.com/channel/UCkExyZZOlvhZv_3h_HaPYYw"; }},
+    { path: '/:pathMatch(.*)*', component: PageNotFound }
+]
+});
 
 export default router
